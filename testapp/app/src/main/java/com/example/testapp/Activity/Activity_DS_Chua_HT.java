37919b45ead6fa478efbfce6f7891a72cedb92d8
Minh Cuong <vmccuong1997@gmail.com>
2019-05-28 01:23:48 +0700

    code moi

--- testapp/app/src/main/java/com/example/testapp/Activity/Activity_DS_Chua_HT.java
+++ testapp/app/src/main/java/com/example/testapp/Activity/Activity_DS_Chua_HT.java
@@ -7,7 +7,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -83,6 +86,28 @@ public class Activity_DS_Chua_HT extends AppCompatActivity {
                 return false;
             }
         });
+        Toolbar toolbar=findViewById(R.id.toolbar_ds_cv_cht);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.right_menu_refresh, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.right_menu_refresh:
+                Intent intent=this.getIntent();
+                finish();
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
